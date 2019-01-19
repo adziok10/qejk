e82ac55b6e16d62d7b55daa492610d9bd24f57b6
Adrian Karwowski <adziok1998@gmail.com>
2019-01-19 12:24:25 +0100

    Update config ready to use imgur api

--- backend/config/example-app.config.js
+++ backend/config/example-app.config.js
@@ -3,4 +3,5 @@ module.exports = {
     jwt_expires_in: 3600 * 24,
     port: 3000 || process.env.PORT,
     image_destination: 'imgur', // 'imgur' and 'node' (save on '/uploads' in your app folder)
+    imgur_ID: 'imurid',
 };
