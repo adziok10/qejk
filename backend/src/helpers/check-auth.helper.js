4296dac72b9d2ffa75276b322dcadfd8b30de10b
Adrian Karwowski <adziok1998@gmail.com>
2019-01-20 21:07:12 +0100

    Refactoring check auth

--- backend/src/helpers/check-auth.helper.js
+++ backend/src/helpers/check-auth.helper.js
@@ -1,15 +1,15 @@
-const jwt = require('jsonwebtoken');
-const config = require('../config/app.config');
+import jwt from 'jsonwebtoken';
+import config from '../config/app.config';
 
 module.exports = (req, res, next) => {
     try {
         const token =  req.headers.authorization.split(" ")[1];
         const decoded = jwt.verify(token, config.jwt_secret);
         req.jwt_data = decoded;
+        next();
     } catch (error) {
         return res.status(401).json({
             message: 'Auth failed'
         });
     }
-    next();
 };
